827338d2b96ec66c55d12081ed4b42935d013e83
Christopher Bannon <ceb6086@psu.edu>
2023-11-24 21:17:37 -0500

    Add project files.

---
+++ A424CEBProject.cpp
@@ -0,0 +1,13 @@
+﻿// A424CEBProject.cpp : Defines the entry point for the application.
+//
+
+#include "A424CEBProject.h"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
